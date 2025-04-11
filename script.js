b3c1e59c49bf79db379e10a40506a1567410a01a
ahamad236 <ahmadshaik4u@gmail.com>
2025-04-11 20:57:14 +0530

    done dragging data into dropZone

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ const dropZone = document.getElementById('dropZone');
 draggables.forEach(draggable => {
     draggable.addEventListener('dragstart', function (event) {
         event.dataTransfer.setData("text/plain", event.target.textContent);
+        console.log('dragstart', event.target.textContent);
     })
 })
 
 dropZone.addEventListener('dragover', function (event) {
     event.preventDefault();
+})
+
+dropZone.addEventListener('drop', function (event) {
     const text = event.dataTransfer.getData("text/plain");
+    console.log('dragover', text);
     dropZone.value = text;
 })
 
